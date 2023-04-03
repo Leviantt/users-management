68804f7127b4309dce3f6ccce316688211a6eae0
Lev <nefedov.lewa@gmail.com>
2023-04-04 02:38:51 +0400

    fix: import jwtModule

--- src/profiles/profiles.module.ts
+++ src/profiles/profiles.module.ts
@@ -5,10 +5,15 @@ import { UsersModule } from 'src/users/users.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from 'src/users/users.model';
 import { Profile } from './profiles.model';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [ProfilesController],
   providers: [ProfilesService],
-  imports: [SequelizeModule.forFeature([Profile, User]), UsersModule],
+  imports: [
+    SequelizeModule.forFeature([Profile, User]),
+    UsersModule,
+    JwtModule,
+  ],
 })
 export class ProfilesModule {}
--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -5,11 +5,12 @@ import { RolesService } from './roles.service';
 import { Role } from './roles.model';
 import { User } from 'src/users/users.model';
 import { UserRoles } from './user-roles.model';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [RolesController],
   providers: [RolesService],
-  imports: [SequelizeModule.forFeature([Role, User, UserRoles])],
+  imports: [SequelizeModule.forFeature([Role, User, UserRoles]), JwtModule],
   exports: [RolesService],
 })
 export class RolesModule {}
